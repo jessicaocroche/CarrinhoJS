3acb0db48befa868f1a5c6857be61bf11fc5098b
jessicaocroche <jessicaocroche@gmail.com>
2024-09-19 14:43:29 -0300

    first commit

---
+++ js/cart.js
@@ -0,0 +1,19 @@
+var valorTotal = [0,0];
+var valorProduto = [50.00,30.00];
+var qtd = [0,0];
+
+function adicionarItem(item){
+  var quantidade = document.getElementById("quantidade" + item);
+  var total = document.getElementById('total' + item);
+  qtd[item] += 1;
+  valorTotal[item] = Number.parseFloat(valorProduto[item]) * qtd[item];
+  quantidade.innerHTML = qtd[item];
+  total.innerHTML = valorTotal[item];
+
+}
+
+function removerItem(item){
+  if(qtd[item] > 0){
+    qtd[item] -=1
+  }
+}
